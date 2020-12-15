5e077f48087082c547de2f01c8c0fc716a40c6c5
Boghian Gabriel <boghiangabriel96@yahoo.com>
2020-12-15 13:37:43 +0200

    add btn decrease

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
+      This is a mini-project in React JS
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{counter}</h1>
+        <div className="div-btn">
+          <Button variant="outlined" color="primary" onClick={() => { setCounter(counter + 1) }}>
+          Increase
+          </Button>
+          <Button variant="outlined" onClick={() => {  setCounter(counter * 0) }}>Reset</Button>
+      
+          <Button variant="outlined" color="secondary" onClick={() => {  setCounter(counter - 1) }}>
+              Decrease
+        </Button>
+      </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
